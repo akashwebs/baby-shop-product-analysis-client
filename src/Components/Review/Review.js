a8a09f54cda80813f8137f83086212023375626b
akashwebs <aksah.shil227@gmail.com>
2022-04-04 18:33:38 +0600

    done reviews section home and reveiws

---
+++ src/Components/Review/Review.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Review.css'
+
+const Review = ({ review }) => {
+    const { name, comment, image, rating } = review;
+    return (
+        <div className='px-10 py-6 bg-[#120E43] text-white rounded-lg'>
+            <div className='review-comment bg-white text-gray-700 text-sm rounded-lg p-8'>
+                <p>{comment}</p>
+            </div>
+            <div className='flex  gap-5 items-center'>
+                <div className=''>
+                <img src={image} alt="" className="w-14 h-14 rounded-full" />
+                </div>
+                <div className="">
+                    <h3 className='text-2xl font-semibold mb-1'>{name}</h3>
+                    <p className=''><small>developer</small></p>
+                    <p> Rating: {rating}</p>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Review;
--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import useReviews from '../../hooks/useReviews';
+import Review from '../Review/Review';
 
 const Reviews = () => {
+    const [reviews, setReviews]=useReviews();
+    const {pathname}=useLocation();
+    console.log(pathname)
+    let mainRieves;
+   
+        if(pathname.includes('reviews')){
+        
+            mainRieves=[...reviews];
+            
+        }else if(pathname.includes('/')){
+            mainRieves=[...reviews.slice(0,3)];
+        }
+    
+    
     return (
         <div>
-            <h2>this is reviews page</h2>
+            <h3 className='text-5xl uppercase font-bold text-center my-5'>Reviews</h3>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-12'>
+                {
+
+                    mainRieves.map(review=><Review
+                    key={review.id}
+                    review={review}
+                    ></Review>)
+                }
+            </div>
         </div>
     );
 };
