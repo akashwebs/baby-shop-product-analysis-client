54c348489e5a612f8d191e62fa6a95a7f6c72605
akashwebs <aksah.shil227@gmail.com>
2022-04-04 20:36:05 +0600

    fix see all reviews button and review title

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import useReviews from '../../hooks/useReviews';
 import Review from '../Review/Review';
 
 const Reviews = () => {
     const [reviews, setReviews]=useReviews();
     const {pathname}=useLocation();
-    console.log(pathname)
     let mainRieves;
+    const navigate=useNavigate();
    
         if(pathname.includes('reviews')){
         
@@ -19,8 +19,8 @@ const Reviews = () => {
     
     
     return (
-        <div>
-            <h3 className='text-5xl uppercase font-bold text-center my-5'>Reviews</h3>
+        <div className='mb-12'>
+            <h3 className='text-5xl uppercase font-bold text-center my-5' style={pathname.includes('reviews')? {display:'none'}: {display:'block'}}>Reviews</h3>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-12'>
                 {
 
@@ -29,6 +29,9 @@ const Reviews = () => {
                     review={review}
                     ></Review>)
                 }
+            </div>
+            <div className='text-center' style={pathname.includes('reviews') ? {display:'none'} : {display:'block'}}>
+            <button onClick={()=>navigate('/reviews')} className='px-10 py-2 rounded-lg  bg-[#120E43] text-xl text-white mt-8 '>See All Reviews</button>
             </div>
         </div>
     );
